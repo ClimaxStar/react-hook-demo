f8501b2f6f2fd87d41c888080a1540ef4dfa8b8b
ClimaxStar <sunnyskai.fellow@gmail.com>
2024-08-23 17:59:01 -0400

    useTransition

---
+++ useTransition/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import TabButton from './TabButton.js';
+import AboutTab from './AboutTab.js';
+import PostsTab from './PostsTab.js';
+import ContactTab from './ContactTab.js';
+
+export default function TabContainer() {
+  const [tab, setTab] = useState('about');
+  return (
+    <>
+      <TabButton
+        isActive={tab === 'about'}
+        onClick={() => setTab('about')}
+      >
+        About
+      </TabButton>
+      <TabButton
+        isActive={tab === 'posts'}
+        onClick={() => setTab('posts')}
+      >
+        Posts (slow)
+      </TabButton>
+      <TabButton
+        isActive={tab === 'contact'}
+        onClick={() => setTab('contact')}
+      >
+        Contact
+      </TabButton>
+      <hr />
+      {tab === 'about' && <AboutTab />}
+      {tab === 'posts' && <PostsTab />}
+      {tab === 'contact' && <ContactTab />}
+    </>
+  );
+}
